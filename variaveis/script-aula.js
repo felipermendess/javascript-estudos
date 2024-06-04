2b0bed63081d4814b026f4d190359ab097090e30
felipermendess <felipermendes04@gmail.com>
2024-06-04 10:07:45 -0400

    ajuste no conteudo variaveis

--- variaveis/script-aula.js
+++ variaveis/script-aula.js
@@ -7,13 +7,22 @@ console.log('Teste');
 //PALAVRAS RESERVADAS
 //NÚMEROS
 
+// Inválido
+// var §nome;
+// var function;
+// var 1possuiFaculdade;
+// Válido
+var $selecionar;
+var _nome;
+var possuiFaculdadeNoExterior;
+
+//VARIÁVEIS - > 
+//Responsáveis ​por salvar dados na memória.
 //var, let, const (Mutação regra)
 var aula = 'Origamid';
 aula = 'Origamid 2.0';
 console.log(aula);
-
 let fruta = 'Maçã';
-
 const faculdade = 'Unopar';
 
 //Comentários => '/**/' ou // 
@@ -31,3 +40,14 @@ var total = compra * preco;
 //Sem definição = Undefined
 var escola;
 console.log(escola);
+
+//VÍRGULA 
+//Utilize a vírgula para criar mais de uma variável, sem repetir a palavra chave var.
+var nome = 'André',
+    idade = 28,
+    possuiFaculdade = true;
+
+
+//Erro ao tentar utilizar uma variável que não foi declarada.
+console.log(teste);
+// retorna teste is not defined
