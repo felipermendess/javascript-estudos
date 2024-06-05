a68bcbbbbc7e49531eea741ce973257c76d73cd5
felipermendess <felipermendes04@gmail.com>
2024-06-05 10:37:39 -0400

    atribuicao-e-ternario

---
+++ atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,24 @@
+//Comentários
+// Comentário de uma linha
+
+/*
+Comentário
+com diversas
+linhas
+*/
+
+//Operadores de atribuição - Abreviação
+var x = 5;
+var y = 10;
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **= y; // x = x ** y (9765625)
+
+//Operador Ternário
+var idade = 19;
+var maiorDeIdade = (idade >= 18) ? 'Maior de idade' : 'Menor de idade';
+console.log(maiorDeIdade);
+// condição ? true : false
