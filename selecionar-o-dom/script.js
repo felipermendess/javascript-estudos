58a58bb8466541b6d16a05bc814c90b1e6e52364
felipermendess <felipermendes04@gmail.com>
2024-06-11 11:20:08 -0400

    selecionar-o-dom 2

--- selecionar-o-dom/script.js
+++ selecionar-o-dom/script.js
@@ -1,41 +1,19 @@
-//ID - getElementById seleciona e retorna elementos do DOM
-// Seleciona pelo ID
-const animaisSection = document.getElementById('animais');
-const contatoSection = document.getElementById('contato');
-// Retorna null caso não exista
-const naoExiste = document.getElementById('teste');
-console.log(animaisSection);
+// Retorne no console todas as imagens do site
+const imagens = document.querySelectorAll('img');
+console.log(imagens);
 
-//CLASSE E TAG - getElementsByClassName e getElementsByTagName selecionam e retornam uma lista de elementos do DOM. A lista retornada está ao vivo, significa que se elementos forem adicionados, ela será automaticamente atualizada.
-// Seleciona pela classe, retorna uma HTMLCollection
-const gridSection = document.getElementsByClassName('grid-section');
-const contato = document.getElementsByClassName('grid-section contato');
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const imgs = document.querySelectorAll('img[src^="img/imagem"]');
+console.log(imgs);
 
-// Seleciona todas as UL's, retorna uma HTMLCollection
-const ul = document.getElementsByTagName('ul');
+// Selecione todos os links internos (onde o href começa com #)
+const linksInternos = document.querySelectorAll('[href^="#"]');
+console.log(linksInternos);
 
-console.log(gridSection);
-console.log(contato);
-console.log(ul);
+// Selecione o primeiro h2 dentro de .animais-descricao
+const primeiroH2 = document.querySelector('.animais-descricao h2');
+console.log(primeiroH2);
 
-// Retorna o primeiro elemento
-console.log(gridSection[0]);
-
-//SELETOR GERAL - querySelector retorna o primeiro elemento que combinar com o seu seletor CSS.
-const animais = document.querySelector('.animais');
-const contact = document.querySelector('#contato');
-const ultimoItem = document.querySelector('.animais-lista li:last-child');
-const linkCSS = document.querySelector('[href^="https://"]');
-const primeiroUl = document.querySelector('ul');
-
-// Busca dentro do Ul apenas
-const navItem = primeiroUl.querySelector('li');
-
-//SELETOR GERAL LISTA - querySelectorAll retorna todos os elementos compatíveis com o seletor CSS em uma NodeList. Diferente do getElementsByClassName, a lista aqui é estática
-const gridsection = document.querySelectorAll('.grid-section');
-const listas = document.querySelectorAll('ul');
-const titulos = document.querySelectorAll('.titulo');
-const fotosAnimais = document.querySelectorAll('.animais-lista img');
-
-// Retorna o segundo elemento
-console.log(gridsection);
+// Selecione o último p do site
+const ultimoParagrafo = document.querySelectorAll('p');
+console.log(ultimoParagrafo[ultimoParagrafo.length - 1]);
