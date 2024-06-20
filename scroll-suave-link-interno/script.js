22f9f686ee49395050166b4bc8172201955b4273
felipermendess <felipermendes04@gmail.com>
2024-06-19 20:56:26 -0400

    scroll-suave-link-interno

--- scroll-suave-link-interno/script.js
+++ scroll-suave-link-interno/script.js
@@ -38,5 +38,28 @@ function initAccordion(){
     })
   }
 }
-
 initAccordion();
+
+function ativaScroll(){
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  function scrollToSection(e){
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+    //MANEIRA ALTERNATIVA
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // });
+  }
+
+  linksInternos.forEach(link => {
+    link.addEventListener('click', scrollToSection);
+  })
+}
+ativaScroll();
