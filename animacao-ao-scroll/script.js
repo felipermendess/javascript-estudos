507fbc069def2bafb8b14060e2feb17b09ca2d24
felipermendess <felipermendes04@gmail.com>
2024-06-20 11:53:29 -0400

    animacao-ao-scroll

--- animacao-ao-scroll/script.js
+++ animacao-ao-scroll/script.js
@@ -63,3 +63,25 @@ function ativaScroll(){
   })
 }
 ativaScroll();
+
+function animacaoAoScroll(){
+  const sections = document.querySelectorAll('.js-scroll');
+  if(sections.length){
+    const windowMetade = window.innerHeight * 0.6;
+    function animaScroll(){
+      sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSection = (sectionTop - windowMetade) < 0;
+        if(isSection){
+          section.classList.add('ativo');
+        }else{
+          section.classList.remove('ativo');
+        }
+      })
+    }
+    animaScroll();
+  
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+animacaoAoScroll();
