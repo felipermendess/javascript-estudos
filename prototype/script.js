bca44b32b1ffb39b8b69822ceb95aa3b99f023a0
felipermendess <felipermendes04@gmail.com>
2024-06-26 22:02:09 -0400

    prototype 3

---
+++ prototype/script.js
@@ -0,0 +1,38 @@
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+function Pessoas(nome,sobrenome,idade){
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
+}
+
+Pessoas.prototype.nomeCompleto = function(){
+  return this.nome + ' ' + this.sobrenome;
+}
+
+const felipe = new Pessoas('Felipe','Rodrigues Mendes Valente',19);
+
+// Liste os métodos acessados por 
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+Object.getOwnPropertyNames(NodeList.prototype);
+Object.getOwnPropertyNames(HTMLCollection.prototype);
+Object.getOwnPropertyNames(Document.prototype);
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+
+li;//HtmlliElement
+li.click;//Function
+li.innerText;//String
+li.value;//Number
+li.hidden;//Boolean
+li.offsetLeft;//Number
+li.click();//Undefined
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name;//String
