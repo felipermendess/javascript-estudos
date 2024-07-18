1368a28ea4fb270b1c40a65a271076877d29ef97
felipermendess <felipermendes04@gmail.com>
2024-07-18 18:00:49 -0400

    criacao do diretorio modal

---
+++ modal/js/modulos/animacao-scroll.js
@@ -0,0 +1,21 @@
+export default function animacaoAoScroll(){
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  if(sections.length){
+    const windowMetade = window.innerHeight * 0.6;
+    function animaScroll(){
+      sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSection = (sectionTop - windowMetade) < 0;
+        if(isSection){
+          section.classList.add('ativo');
+        }else{
+          section.classList.remove('ativo');
+        }
+      })
+    }
+    animaScroll();
+  
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+animacaoAoScroll();
---
+++ modal/js/script.js
@@ -0,0 +1,9 @@
+import ativaScroll from "./modulos/scroll-suave.js";
+import animacaoAoScroll from "./modulos/animacao-scroll.js";
+import initAccordion from "./modulos/accordion.js";
+import initTab from "./modulos/tabnav.js";
+
+ativaScroll();
+animacaoAoScroll();
+initAccordion();
+initTab();
