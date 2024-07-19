66b0d514eebe2587ed207749f5b7f4560fe0286f
felipermendess <felipermendes04@gmail.com>
2024-07-19 18:09:15 -0400

    modal 2

--- modal/js/script.js
+++ modal/js/script.js
@@ -2,8 +2,10 @@ import ativaScroll from "./modulos/scroll-suave.js";
 import animacaoAoScroll from "./modulos/animacao-scroll.js";
 import initAccordion from "./modulos/accordion.js";
 import initTab from "./modulos/tabnav.js";
+import initModal from "./modulos/modal.js";
 
 ativaScroll();
 animacaoAoScroll();
 initAccordion();
 initTab();
+initModal();
