ef9a843271d3aedaf2553fb38b37498f0dec706f
felipermendess <felipermendes04@gmail.com>
2024-07-22 10:55:51 -0400

    criação do diretorio tooltip

---
+++ tooltip/js/script.js
@@ -0,0 +1,13 @@
+import ativaScroll from "./modulos/scroll-suave.js";
+import animacaoAoScroll from "./modulos/animacao-scroll.js";
+import initAccordion from "./modulos/accordion.js";
+import initTab from "./modulos/tabnav.js";
+import initModal from "./modulos/modal.js";
+import initTooltip from "./modulos/tooltip.js";
+
+ativaScroll();
+animacaoAoScroll();
+initAccordion();
+initTab();
+initModal();
+initTooltip();
