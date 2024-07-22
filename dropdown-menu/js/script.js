124b78291fe1d2d170bf45d1e7e7fec894f518a4
felipermendess <felipermendes04@gmail.com>
2024-07-22 19:06:41 -0400

    criação do diretorio dropdown-menu

---
+++ dropdown-menu/js/script.js
@@ -0,0 +1,15 @@
+import ativaScroll from "./modulos/scroll-suave.js";
+import animacaoAoScroll from "./modulos/animacao-scroll.js";
+import initAccordion from "./modulos/accordion.js";
+import initTab from "./modulos/tabnav.js";
+import initModal from "./modulos/modal.js";
+import initTooltip from "./modulos/tooltip.js";
+import initDropdownMenu from "./modulos/dropdown-menu.js";
+
+ativaScroll();
+animacaoAoScroll();
+initAccordion();
+initTab();
+initModal();
+initTooltip();
+initDropdownMenu();
