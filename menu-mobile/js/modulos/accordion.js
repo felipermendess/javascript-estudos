37b8ab5e071b4a8539aa1c9d976d7f4dfb9eebc3
felipermendess <felipermendes04@gmail.com>
2024-07-25 18:30:46 -0400

    criação do diretorio menu-mobile

---
+++ menu-mobile/js/modulos/accordion.js
@@ -0,0 +1,17 @@
+export default function initAccordion(){
+  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
+  const classeAtivo = 'ativo';
+  if(accordionList.length){
+    accordionList[0].classList.add(classeAtivo);
+    accordionList[0].nextElementSibling.classList.add(classeAtivo);
+
+    function ativaLista(){
+      this.classList.toggle(classeAtivo);
+      this.nextElementSibling.classList.toggle(classeAtivo);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', ativaLista)
+    })
+  }
+}
