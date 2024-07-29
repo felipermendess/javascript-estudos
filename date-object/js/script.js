925064abbbd6a4d6f3aae3d0a94b0eace2035af9
felipermendess <felipermendes04@gmail.com>
2024-07-29 17:46:13 -0400

    criação do diretorio data-object

---
+++ date-object/js/script.js
@@ -0,0 +1,19 @@
+import ativaScroll from "./modulos/scroll-suave.js";
+import animacaoAoScroll from "./modulos/animacao-scroll.js";
+import initAccordion from "./modulos/accordion.js";
+import initTab from "./modulos/tabnav.js";
+import initModal from "./modulos/modal.js";
+import initTooltip from "./modulos/tooltip.js";
+import initDropdownMenu from "./modulos/dropdown-menu.js";
+import initMenuMobile from "./modulos/menu-mobile.js";
+import initAnimaNumeros from "./modulos/anima-numeros.js";
+
+ativaScroll();
+animacaoAoScroll();
+initAccordion();
+initTab();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
