196b35b02a57abf31aa2c86775d13e6aa10211bd
felipermendess <felipermendes04@gmail.com>
2024-08-23 10:27:29 -0400

    inserindo arquivos e conteúdos finais para melhor compreensão do tema fetch

--- fetch/script-aula.js
+++ fetch/script-aula.js
@@ -46,6 +46,58 @@
 //   document.querySelector('h1').innerText = titulo.innerText;
 // });
 
+//BLOB - Um blob é um tipo de objeto utilizado para representação de dados de um arquivo. O blob pode ser utilizado para transformarmos requisições de imagens por exemplo. O blob gera um URL único.
+// const div = document.createElement('div');
+// fetch('./imagem.png')
+// .then(response => response.blob())
+// .then(imgBlob => {
+//   const blobUrl = URL.createObjectURL(imgBlob);
+//   console.log(blobUrl);
+// });
+
+//CLONE - Ao utilizarmos os métodos acima, text, json e blob, a resposta é modificada. Por isso existe o método clone, caso você necessite transformar uma resposta em diferentes valores.
+// const div = document.createElement('div');
+// fetch('https://viacep.com.br/ws/01001000/json/')
+// .then(response => {
+//   const cloneResponse = response.clone();
+//   response.json().then(json => {
+//     console.log(json)
+//   });
+//   cloneResponse.text().then(text => {
+//     console.log(text)
+//   });
+// });
+
+//HEADERS - É uma propriedade que possui os cabeçalhos da requisição. É um tipo de dado iterável então podemos utilizar o forEach para vermos cada um deles.
+// const div = document.createElement('div');
+// fetch('https://viacep.com.br/ws/01001000/json/')
+// .then(response => {
+//   response.headers.forEach(console.log);
+// });
+
+//STATUS E OK - Retorna o status da requisição. Se foi 404, 200, 202 e mais. ok retorna um valor booleano sendo true para uma requisição de sucesso e false para uma sem sucesso.
+// const div = document.createElement('div');
+// fetch('https://viacep.com.br/ws/01001000/json/')
+// .then(response => {
+//   console.log(response.status, response.ok);
+//   if(response.status === 404) {
+//     console.log('Página não encontrada')
+//   }
+// });
+
+//URL E TYPE - .url retorna o url da requisição. .type retorna o tipo da reposta.
+// const div = document.createElement('div');
+// fetch('https://viacep.com.br/ws/01001000/json/')
+// .then(response => {
+//   console.log(response.type, response.url);
+// });
+// //types
+// basic: feito na mesma origem
+// cors: feito em url body pode estar disponível
+// error: erro de conexão
+// opaque: no-cors, não permite acesso de outros sites
+
+
 //FAZENDO A REQUISIÇÃO DE ARQUIVO INTERNO DE TEXTO COM FETCH E INSERINDO EM UMA DIV
 const div = document.querySelector('div');
 // const requisicao = fetch('./doc.txt');
@@ -73,13 +125,51 @@ const div = document.querySelector('div');
 //   document.body.appendChild(criandoElemento);
 // })
 
-const div2 = document.createElement('div');
+//TRANSFORMANDO TODO HTML EM TEXTO E COLOCANDO NUMA DIV COM INNERHTML
+// const div2 = document.createElement('div');
+// fetch('./sobre.html')
+// .then(response => {
+//   return response.text();
+// }).then(html => {
+//   div2.innerHTML = html;
+//   const titulo = div2.querySelector('h1');
+//   document.querySelector('h1').innerText = titulo.innerText;
+// })
 
-fetch('./sobre.html')
-.then(response => {
-  return response.text();
-}).then(html => {
-  div2.innerHTML = html;
-  const titulo = div2.querySelector('h1');
-  document.querySelector('h1').innerText = titulo.innerText;
-})
+//USANDO O BLOB PARA FAZER REQUISIÇÃO DE IMAGEM
+// const imagem = fetch('./imagem.png');
+// imagem.then(response => response.blob())
+// .then(body => {
+//   const blobUrl = URL.createObjectURL(body);
+//   const image = document.querySelector('img');
+//   image.src = blobUrl;
+// })
+
+//CLONANDO RESPOSTA PARA OBTER RESPOSTA SECUNDÁRIA COM OUTRO FORMATO(JSON)
+// const cep = fetch('https://viacep.com.br/ws/01001000/json/');
+// cep.then(response => {
+//   const responseSecundaria = response.clone();
+//   response.text().then(text => console.log(text));
+//   responseSecundaria.json().then(json => console.log(json));
+// })
+
+//ITERANDO SOBRE OS HEADERS DA REQUISIÇÃO
+// const cep = fetch('https://viacep.com.br/ws/01001000/json/');
+// cep.then(response => {
+//   response.headers.forEach(console.log); //Iterando sobre headers do objeto Response
+// })
+
+//VERIFICANDO STATUS DA PÁGINA
+// const documentos = fetch('./docs.txt');
+// documentos.then(response => {
+//   console.log(response.status);
+//   if(response.status === 404)
+//     console.log('Página não encontrada');
+// })
+
+//PEGANDO A URL E TIPO DA RESPONSE
+// const documentos = fetch('./doc.txt');
+// documentos.then(response => {
+//   console.log(response.url);
+//   console.log(response.type);
+// })
