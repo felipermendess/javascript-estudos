76373244a8a0900e8fa1be9c31a1f2a73d78a701
felipermendess <felipermendes04@gmail.com>
2024-09-18 09:33:21 -0400

    Criação do diretório extends com arquivos index e script

---
+++ extends/script.js
@@ -0,0 +1,19 @@
+//SubClasses  - É possível criarmos uma subclasse, esta irá ter acesso aos métodos da classe à qual ela estendeu através do seu protótipo.
+// class Veiculo {
+//     constructor(rodas) {
+//       this.rodas = rodas;
+//     }
+//     acelerar() {
+//       console.log('Acelerou');
+//     }
+//   }
+  
+//   class Moto extends Veiculo {
+//     empinar() {
+//       console.log('Empinou com ' + this.rodas + ' rodas');
+//     }
+//   }
+  
+//   const honda = new Moto(2);
+//   honda.empinar();
+  
