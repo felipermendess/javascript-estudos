58dcc2aa1d92232d2367d12b16aa884c1c65a51f
felipermendess <felipermendes04@gmail.com>
2024-11-07 10:23:52 -0400

    regexp 8

--- regex/script-aula.js
+++ regex/script-aula.js
@@ -510,6 +510,102 @@
 //   console.log(regexpResult[0]);
 // }
 
+//Match - O match() é um método de strings que pode receber como argumento uma Regexp. Existe uma diferença de resultado quando utilizamos a flag g ou não.
+// const regexp = /\w{2,}/g;
+// const regexpSemG = /\w{2,}/;
+// const frase = 'JavaScript, TypeScript e CoffeeScript';
+
+// frase.match(regexp);
+// // ['JavaScript', 'TypeScript', 'CoffeeScript']
+
+// frase.match(regexpSemG);
+// // ["JavaScript", index: 0, input: "JavaScript,
+// // TypeScript e CoffeeScript", groups: undefined]
+
+//Split - O split serve para distribuirmos uma string em uma array, quebrando a string no argumento que for passado. Este método irá remover o match da array final.
+// const frase = 'JavaScript, TypeScript, CoffeeScript';
+
+// frase.split(', ');
+// // ["JavaScript", "TypeScript", "CoffeeScript"]
+// frase.split(/Script/g);
+// // ["Java", ", Type", ", Coffee", ""]
+
+// const tags = `
+// <ul>
+//   <li>Item 1</li>
+//   <li>Item 2</li>
+// </ul>
+// `;
+
+// tags.split(/(?<=<\/?)\w+/g).join('div');
+// // <div>
+// //   <div>Item 1</div>
+// //   <div>Item 2</div>
+// // <div>
+
+//Replace - O método replace() é o mais interessante por permitir a utilização de funções de callback para cada match que ele der com a Regexp.
+// const tags = `
+// <ul>
+//   <li>Item 1</li>
+//   <li>Item 2</li>
+// </ul>
+// `;
+
+// tags.replace(/(?<=<\/?)\w+/g, 'div');
+// // <div>
+// //   <div>Item 1</div>
+// //   <div>Item 2</div>
+// // <div>
+
+//Captura - É possível fazer uma referência ao grupo de captura dentro do argumento do replace. Então podemos utilizar $&, $1 e mais.
+// const tags = `
+// <ul>
+//   <li>Item 1</li>
+//   <li>Item 2</li>
+// </ul>
+// `;
+
+// tags.replace(/<li/g, '$& class="ativo"');
+// // <ul>
+// //   <li class="ativo">Item 1</li>
+// //   <li class="ativo">Item 2</li>
+// // </ul>
+
+//Grupos de captura - É possível definirmos quantos grupos de captura quisermos.
+// const emails = `
+// empresa@email.com
+// contato@email.com
+// suporte@email.com
+// `;
+
+// emails.replace(/(\w+@)[\w.]+/g, '$1gmail.com');
+// // empresa@gmail.com
+// // contato@gmail.com
+// // suporte@gmail.com
+
+//Callback - Para substituições mais complexas, podemos utilizar um callback como segundo argumento do replace. O valor do return será o que irá substituir cada match.
+// const regexp = /(\w+)(@[\w]+)/g;
+// const emails = `joao@homail.com.br
+// marta@ggmail.com.br
+// bruna@oulook.com.br`
+
+// emails.replace(regexp, function(...args) {
+//   console.log(args);
+//   if(args[2] === '@homail') {
+//     return `${args[1]}@hotmail`;
+//   } else if(args[2] === '@ggmail') {
+//     return `${args[1]}@gmail`;
+//   } else if(args[2] === '@oulook') {
+//     return `${args[1]}@outlook`;
+//   } else {
+//     return 'x';
+//   }
+// });
+
+// joao@hotmail.com.br
+// marta@gmail.com.br
+// bruna@outlook.com.br
+
 //Testes
 const frase = 'PHP'; //sentido literal - p seguido de h e p
 const novaFrase = frase.replace(/P/, 'J')
@@ -759,3 +855,85 @@ let regexpResult;
 while((regexpResult = regexp40.exec(frase8)) !== null) {
   console.log(regexpResult[0]);
 }
+
+//match
+const regexp41 = /\w{2,}/g;
+const regexpSemG = /\w{2,}/;
+const frase9 = 'JavaScript, TypeScript e CoffeeScript';
+
+frase9.match(regexp41);
+// ['JavaScript', 'TypeScript', 'CoffeeScript']
+
+frase9.match(regexpSemG);
+// ["JavaScript", index: 0, input: "JavaScript,
+// TypeScript e CoffeeScript", groups: undefined]
+
+//split
+const frase10 = 'JavaScript, TypeScript, CoffeeScript';
+frase10.split(', ');
+// ["JavaScript", "TypeScript", "CoffeeScript"]
+frase10.split(/Script/g);
+// ["Java", ", Type", ", Coffee", ""]
+
+//replace
+const tagsss = `
+<ul>
+  <li>Item 1</li>
+  <li>Item 2</li>
+</ul>
+`;
+
+tagsss.replace(/(?<=<\/?)\w+/g, 'div');
+// <div>
+//   <div>Item 1</div>
+//   <div>Item 2</div>
+// <div>
+
+//captura
+const tagssss = `
+<ul>
+  <li>Item 1</li>
+  <li>Item 2</li>
+</ul>
+`;
+
+tagssss.replace(/<li/g, '$& class="ativo"');
+// <ul>
+//   <li class="ativo">Item 1</li>
+//   <li class="ativo">Item 2</li>
+// </ul>
+
+//grupos de captura
+const emailss = `
+empresa@email.com
+contato@email.com
+suporte@email.com
+`;
+
+emailss.replace(/(\w+@)[\w.]+/g, '$1gmail.com');
+// empresa@gmail.com
+// contato@gmail.com
+// suporte@gmail.com
+
+//callback
+const regexp42 = /(\w+)(@[\w]+)/g;
+const emailsss = `joao@homail.com.br
+marta@ggmail.com.br
+bruna@oulook.com.br`
+
+emailsss.replace(regexp42, function(...args) {
+  console.log(args);//grupos de captura estão aqui
+  if(args[2] === '@homail') {
+    return `${args[1]}@hotmail`;
+  } else if(args[2] === '@ggmail') {
+    return `${args[1]}@gmail`;
+  } else if(args[2] === '@oulook') {
+    return `${args[1]}@outlook`;
+  } else {
+    return 'x';
+  }
+});
+// joao@hotmail.com.br
+// marta@gmail.com.br
+// bruna@outlook.com.br
+
